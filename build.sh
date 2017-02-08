b3714cdba2a1f0d775deed01b80ec1bc4d8c0851
David Santia <davidsantia.morris@gmail.com>
2017-02-08 13:45:51 -0700

    Build load app on golang:alpine container

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ PROJECT=github.com/DavidSantia/react-redis-quiztool/load
 # Build for Linux, statically linked
 echo Building load app $GOPATH/src/$PROJECT
 docker run --rm --name golang -v $GOPATH/src:/go/src golang:alpine /bin/sh -l -c \
-    "cd /go/src/$PROJECT; GCGO_ENABLED=0 /usr/local/go/bin/go build -v -i"
+    "cd /go/src/$PROJECT; CGO_ENABLED=0 /usr/local/go/bin/go build -v -i"
 
 docker-compose build
